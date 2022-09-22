2c24b4ae904cf2fd6900b677ba8a09521bf3a7ca
yukinouz <47273687+yukinouz@users.noreply.github.com>
2022-09-22 15:38:07 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gulp For WordPress | yukinouz1</title>
+</head>
+<body>
+  <header>
+    <h1>Gulp For WordPress</h1>
+  </header>
+  <main>
+    <h2>GulpをWordPressで動かすためのサンプルファイル群です。</h2>
+  </main>
+</body>
+</html>
